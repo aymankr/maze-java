4fae4972d98f61dc389d7804689709ccb67d5be1
Ayman Kr <kr.ayman@hotmail.com>
2021-09-16 17:19:12 +0200

    deplacement fluide seulement du heros fini

--- src/main/java/vue2D/sprites/ASprite.java
+++ src/main/java/vue2D/sprites/ASprite.java
@@ -22,14 +22,15 @@ public abstract class ASprite implements ISprite {
     IPersonnage IPerso;
     Image image;
     ILabyrinthe labyrinthe;
-    int x;
-    int y;
+    int pixCumulX;
+    int pixCumulY;
+    int decalageX;
+    int decalageY;
     Salle posPrecedent;
     boolean enDeplacement;
 
     public ASprite(IPersonnage p, ILabyrinthe l) {
-        x = 0;
-        y = 0;
+        pixCumulX = pixCumulY = decalageX = decalageY = 0;
         IPerso = p;
         labyrinthe = l;
         enDeplacement = false;
@@ -41,33 +42,29 @@ public abstract class ASprite implements ISprite {
         Salle position = (Salle) IPerso.getPosition();
         Salle positionChoisie = (Salle) IPerso.faitSonChoix(labyrinthe);
 
-        if (position != positionChoisie) {
+        if (position != positionChoisie && !enDeplacement) {
             posPrecedent = position;
-        }
+            enDeplacement = true;
 
-        int deplaceX = 0;
-        int deplaceY = 0;
-        if (posPrecedent != null) {
             if (posPrecedent.getX() > positionChoisie.getX()) {
-                deplaceX = -1;
+                decalageX = -1;
             } else if (posPrecedent.getX() < positionChoisie.getX()) {
-                deplaceX = 1;
+                decalageX = 1;
             }
 
             if (posPrecedent.getY() > positionChoisie.getY()) {
-                deplaceY = -1;
+                decalageY = -1;
             } else if (posPrecedent.getY() < positionChoisie.getY()) {
-                deplaceY = 1;
+                decalageY = 1;
             }
         }
 
-        if (posPrecedent != null) {
-            enDeplacement = true;
-            setCoordonnees(deplaceX, deplaceY);
-            g.drawImage(image, posPrecedent.getX() * unite + x, posPrecedent.getY() * unite + y, unite, unite);
-            if (posPrecedent.getX() * unite + x == positionChoisie.getX() * unite && posPrecedent.getY() * unite + y == positionChoisie.getY() * unite) {
-                posPrecedent = null;
-                x = y = 0;
+        if (enDeplacement) {
+            setCoordonnees(decalageX, decalageY);
+            g.drawImage(image, posPrecedent.getX() * unite + pixCumulX, posPrecedent.getY() * unite + pixCumulY, unite, unite);
+            if (posPrecedent.getX() * unite + pixCumulX == positionChoisie.getX() * unite
+                    && posPrecedent.getY() * unite + pixCumulY == positionChoisie.getY() * unite) {
+                pixCumulX = pixCumulY = decalageX = decalageY = 0;
                 enDeplacement = false;
             }
         } else {
@@ -77,7 +74,7 @@ public abstract class ASprite implements ISprite {
 
     @Override
     public void setCoordonnees(int xpix, int ypix) {
-        x += xpix;
-        y += ypix;
+        pixCumulX += xpix;
+        pixCumulY += ypix;
     }
 }
